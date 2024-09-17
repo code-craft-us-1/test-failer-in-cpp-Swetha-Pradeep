2b7ee2cc17b911a3215c5b0a08acfa2b2f0ee542
Swetha-Pradeep <swetha.pradeep@philips.com>
2024-09-17 16:02:11 +0530

    added assertion

--- misaligned.cpp
+++ misaligned.cpp
@@ -1,16 +1,40 @@
 #include <assert.h>
 #include <iostream>
-
+#include <map>
+#include <string>
 enum MajorColor{ White = 0, Red, Black, Yellow, Violet };
 enum MinorColor{ Blue = 0, Orange, Green, Brown, Slate };
+const char* majorColor[] = { "White", "Red", "Black", "Yellow", "Violet" };
+const char* minorColor[] = { "Blue", "Orange", "Green", "Brown", "Slate" };
+int numberOfMajorColors =sizeof(majorColor) / sizeof(majorColor[0]);
+int numberOfMinorColors =sizeof(minorColor) / sizeof(minorColor[0]);
 
+std::map <std::string,MajorColor> majorColorEnumtoStringMap;
+std::map <std::string, MinorColor> minorColorEnumtoStringMap;
+
+void mapMajorarraytoEnum() {
+    
+
+    for (int i = 0; i<numberOfMajorColors; i++) {
+        majorColorEnumtoStringMap.insert({ majorColor[i],(MajorColor)i });
+    }
+}
+
+void mapMinorarraytoEnum() {
+
+
+    for (int i = 0; i < numberOfMinorColors; i++) {
+        minorColorEnumtoStringMap.insert({ minorColor[i],(MinorColor)i });
+    }
+}
 int printColorMap() {
-    const char* majorColor[] = { "White", "Red", "Black", "Yellow", "Violet" };
-    const char* minorColor[] = { "Blue", "Orange", "Green", "Brown", "Slate" };
+    
     int i = 0, j = 0;
     for (i = 0; i < 5; i++) {
         for (j = 0; j < 5; j++) {
             std::cout << i * 5 + j << " | " << majorColor[i] << " | " << minorColor[i] << "\n";
+            int pairNumber = mapColorToNumber(majorColorEnumtoStringMap[majorColor[i]], minorColorEnumtoStringMap[minorColor[i]]);
+            assert(pairNumber == (i * 5 + j));
         }
     }
     return i * j;
@@ -21,6 +45,8 @@ int mapColorToNumber(MajorColor majorColor, MinorColor minorColor) {
     return pairNumber;
 }
 int main() {
+    mapMajorarraytoEnum();
+    mapMinorarraytoEnum();
     int pairNumber = mapColorToNumber(MajorColor::White, MinorColor::Orange);
     assert(pairNumber == 1);
     int result = printColorMap();
